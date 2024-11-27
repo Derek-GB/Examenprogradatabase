7e0c3ab850a3140a3f287057e2ffd8bb362091b8
DylanS.S <dsegurasevilla@gmail.com>
2024-11-27 13:40:08 -0600

    Modelo Taller y TallerDTO
    Se creo la clase taller y el DTO de taller

---
+++ Modelo/Taller/TallerDTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.Taller;
+
+/**
+ *
+ * @author DYLAN
+ */
+public class TallerDTO {
+    
+}

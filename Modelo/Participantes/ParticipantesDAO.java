aa0ca53a5753e928ee0955b912d5bcfd47727ea3
Tony <twonejo@gmail.com>
2024-11-27 13:52:43 -0600

    se creo la clase participantesDAO
    se creo la clase participantesDAO y movi la clase porque la deje donde no era

---
+++ Modelo/Participantes/ParticipantesDAO.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo.Participantes;
+
+import Modelo.Dao.Dao;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Student
+ */
+public class ParticipantesDAO extends Dao<ParticipantesDTO> {
+
+    @Override
+    public boolean create(ParticipantesDTO dto) throws SQLException {
+        
+    }
+
+    @Override
+    public ParticipantesDTO read(Object id) throws SQLException {
+     
+    }
+
+    @Override
+    public List<ParticipantesDTO> readAll() throws SQLException {
+      
+    }
+
+    @Override
+    public boolean update(ParticipantesDTO dto) throws SQLException {
+       
+    }
+
+    @Override
+    public boolean delete(Object id) throws SQLException {
+     
+    }
+    
+}
